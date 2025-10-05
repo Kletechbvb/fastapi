b857dd9695a571332722c5d2e5fdad2b0ddf1012
Kletechbvb <157311239+Kletechbvb@users.noreply.github.com>
2025-10-05 12:27:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ app.include_router(chat_router)
 app.include_router(question_router)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
